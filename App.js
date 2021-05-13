e67ee2cdb46b84ac56be9cf0c7db73ee1dc2f32e
johnnie71 <johnnie.agonz71@gmail.com>
2021-05-13 16:29:33 -0400

    importing PlacesNavigator

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import PlacesNavigator from './navigation/PlacesNavigator';
 
 export default function App() {
-  
+
   return (
     
   )
