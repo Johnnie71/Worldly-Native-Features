b6387dff72d4a5c407d0ac15f69bf306d07bebf9
johnnie71 <johnnie.agonz71@gmail.com>
2021-05-13 16:43:11 -0400

    imported newPlaceScreen

--- navigation/PlacesNavigator.js
+++ navigation/PlacesNavigator.js
@@ -2,6 +2,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import PlacesListScreen from '../screens/PlacesListScreen';
 import PlaceDetailScreen from '../screens/PlaceDetailScreen';
+import NewPlaceScreen from '../screens/NewPlaceScreen';
 
 const PlacesNavigator = props => {
 
