36e89cf54d849cec239e598a7538f113199008f4
johnnie71 <johnnie.agonz71@gmail.com>
2021-05-15 14:58:14 -0400

    set up view and text for screen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -3,6 +3,12 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const MapScreen = props => {
 
+    return (
+        <View>
+            <Text>Map Screen</Text>
+        </View>
+    )
+
 };
 
 const styles = StyleSheet.create({
