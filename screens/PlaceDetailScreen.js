b6159b8bf38a1e43bc6f73541d6bb46e111c508d
johnnie71 <johnnie.agonz71@gmail.com>
2021-05-15 14:49:59 -0400

    set up return with view and text for functional component

--- screens/PlaceDetailScreen.js
+++ screens/PlaceDetailScreen.js
@@ -3,6 +3,12 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const PlaceDetailScreen = props => {
 
+    return (
+        <View>
+            <Text>Place Detail Screen</Text>
+        </View>
+    )
+
 };
 
 const styles = StyleSheet.create({
