53bb726f70336a6688d857b4338772a44a9275e9
johnnie71 <johnnie.agonz71@gmail.com>
2021-05-15 14:44:11 -0400

    set up View and Text in functional component

--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -3,6 +3,11 @@ import { View, Text, Stylesheet } from 'react-native';
 
 const PlacesListScreen = props => {
 
+    return (
+        <View>
+            <Text>Places List Screen</Text>
+        </View>
+    )
 };
 
 export default PlacesListScreen;
