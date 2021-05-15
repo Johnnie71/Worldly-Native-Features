a4841bebbe980ed98ae9c7de0da7e2df20906143
johnnie71 <johnnie.agonz71@gmail.com>
2021-05-15 14:57:36 -0400

    set up View and Text within functional component

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -3,6 +3,12 @@ import { View, Text, StyleSheet} from 'react-native';
 
 const NewPlaceScreen = props => {
 
+    return (
+        <View>
+            <Text>New Place Screen</Text>
+        </View>
+    )
+
 };
 
 const styles = StyleSheet.create({
