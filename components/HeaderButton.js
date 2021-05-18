c9dfaab8684890003db28401ece77fecb38da456
johnnie71 <johnnie.agonz71@gmail.com>
2021-05-18 14:32:43 -0400

    adjusted color of button

--- components/HeaderButton.js
+++ components/HeaderButton.js
@@ -11,6 +11,7 @@ const CustomHeaderButton = props => {
                {...props} 
                IconComponent={Ionicons} 
                iconSize={23}
+               color={Colors.primary}
             />
         </View>
     )
