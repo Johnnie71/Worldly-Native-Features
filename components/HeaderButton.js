344e422b3e96ded921cfbd7510a2a48ef08f2335
johnnie71 <johnnie.agonz71@gmail.com>
2021-05-18 14:33:19 -0400

    imported platform

--- components/HeaderButton.js
+++ components/HeaderButton.js
@@ -1,5 +1,6 @@
 import { HeaderButton } from 'react-navigation-header-buttons';
 import { Ionicons } from '@expo/vector-icons';
+import { Platform } from 'react-native';
 
 import Colors from '../constants/Colors';
 
