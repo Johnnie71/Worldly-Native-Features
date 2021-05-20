ae5368c43ba526a9962d5b1ff42deb08175ca323
johnnie71 <johnnie.agonz71@gmail.com>
2021-05-20 14:49:00 -0400

    importing scrollview

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TextInput, StyleSheet} from 'react-native';
+import { ScrollView, View, Text, TextInput, StyleSheet} from 'react-native';
 
 const NewPlaceScreen = props => {
 
