5abab2b6cab1cea1822f65caefbcf25636b4bfe5
johnnie71 <johnnie.agonz71@gmail.com>
2021-05-28 18:15:06 -0400

    imported createStore and combineReducers from redux

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {} from 'redux';
+import { createStore, combineReducers } from 'redux';
 import {} from 'react-redux';
 import PlacesNavigator from './navigation/PlacesNavigator';
 
