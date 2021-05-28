aaf3fcedcaad75618c8f456c911d33b5915fd5b7
johnnie71 <johnnie.agonz71@gmail.com>
2021-05-28 18:18:05 -0400

    importing placesReducer

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import PlacesNavigator from './navigation/PlacesNavigator';
+import placesReducer from './store/places-reducer';
 
 const rootReducer = combineReducers({
-  
+
 });
 
 export default function App() {
