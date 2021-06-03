a02c15ea91dc36678f121931a8b244a05b0f5716
johnnie71 <johnnie.agonz71@gmail.com>
2021-06-03 19:41:28 -0400

    adding new place to the array of places

--- store/places-reducer.js
+++ store/places-reducer.js
@@ -9,6 +9,9 @@ export default (state = initialState, action) => {
     switch (action.type) {
         case ADD_PLACE:
             const newPlace = new Place(new Date().toString(), action.placeData.title);
+            return {
+                places: state.places.concat(newPlace)
+            }
             default:
                 return state;
     }
