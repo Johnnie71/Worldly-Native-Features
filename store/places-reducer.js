3b7914569e56b267155db93da7718aea81c62762
johnnie71 <johnnie.agonz71@gmail.com>
2021-06-03 19:31:01 -0400

    switch case for add place

--- store/places-reducer.js
+++ store/places-reducer.js
@@ -1,7 +1,12 @@
+import { ADD_PLACE } from "./places-actions";
+
 const initialState = {
     places: []
 };
 
 export default (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_PLACE
+    }
     return state;
 }
