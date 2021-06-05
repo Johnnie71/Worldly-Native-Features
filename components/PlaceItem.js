873eeb116eea44588895ac5b62b6ed0ce8084ef5
johnnie71 <johnnie.agonz71@gmail.com>
2021-06-04 21:13:40 -0400

    created functional component

--- components/PlaceItem.js
+++ components/PlaceItem.js
@@ -1,3 +1,7 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import Colors from '../constants/Colors';
+
+const PlaceItem = props => {
+
+};
