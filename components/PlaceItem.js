3102b86a37e510b5a1a73b8735f887a13fdbb4ca
johnnie71 <johnnie.agonz71@gmail.com>
2021-06-05 15:47:03 -0400

    title style

--- components/PlaceItem.js
+++ components/PlaceItem.js
@@ -8,7 +8,7 @@ const PlaceItem = props => {
         <TouchableOpacity onPress={props.onSelect} >
             <Image style={styles.image} source={{ uri: props.image }}/>
             <View style={styles.infoContainer} >
-                <Text>{props.title}</Text>
+                <Text style={styles.title} >{props.title}</Text>
                 <Text>{props.address}</Text>
             </View>
         </TouchableOpacity>
