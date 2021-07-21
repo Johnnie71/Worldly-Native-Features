9d16ed2a272a5cb808ef32ef91feb474c8d6a15c
johnnie71 <johnnie.agonz71@gmail.com>
2021-07-21 17:35:38 -0400

    created button and imported colors to use for button

--- components/ImagePicker.js
+++ components/ImagePicker.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View, Button, Text, StyleSheet } from 'react-native';
+import Colors from '../constants/Colors';
 
 
 const ImagePicker = props => {
     return (
         <View>
             <View><Text>No image selected</Text></View>
+            <Button title="Take Image" color={Colors.primary}/>
         </View>
     )
 };
