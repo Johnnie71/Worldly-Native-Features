63396c3b1a030279c0843c7dae405ac05072afdf
johnnie71 <johnnie.agonz71@gmail.com>
2021-07-22 21:07:15 -0400

    adjusted css for image preview

--- components/ImagePicker.js
+++ components/ImagePicker.js
@@ -27,13 +27,13 @@ const styles = StyleSheet.create({
 
     },
     imagePreview: {
-      width: '100%',
-      height: 200,
-      marginBottom: 10,
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderColor: '#ccc',
-      borderWidth: 1
+       width: '100%',
+       height: 200,
+       marginBottom: 10,
+       justifyContent: 'center',
+       alignItems: 'center',
+       borderColor: '#ccc',
+       borderWidth: 1
     },
     image: {
         width: '100%',
