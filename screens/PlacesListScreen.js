d120b9f1cfc89559005e4d31147e810e10cd910d
johnnie71 <johnnie.agonz71@gmail.com>
2021-09-14 22:56:44 -0400

    adjustments

--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Platform, FlatList, StyleSheet } from 'react-native';
+import { View, Text, Platform, FlatList, StyleSheet, Button } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useSelector } from 'react-redux'
 
@@ -11,24 +11,29 @@ const PlacesListScreen = props => {
     const places = useSelector(state => state.places.places);
 
     return (
-
-        <FlatList
-            data={places}
-            keyExtractor={item => item.id}
-            renderItem={itemData => (
-             <PlaceItem 
-                image={null} 
-                title={itemData.item.title}
-                address={null}
-                onSelect={() => {
-                    props.navigation.navigate('PlaceDetail', {
-                        placeTitle: itemData.item.title, 
-                        placeId: itemData.item.id
-                    });
-                }}
-             />
-            )}
-        />
+        <View style={styles.screen}>
+            <Text>Places List!</Text>
+            <View>
+                <Button title='Add' onPress={() => props.navigation.push('MapScreen')}/>
+            </View>
+        </View>
+        // <FlatList
+        //     data={places}
+        //     keyExtractor={item => item.id}
+        //     renderItem={itemData => (
+        //      <PlaceItem 
+        //         image={null} 
+        //         title={itemData.item.title}
+        //         address={null}
+        //         onSelect={() => {
+        //             props.navigation.navigate('PlaceDetail', {
+        //                 placeTitle: itemData.item.title, 
+        //                 placeId: itemData.item.id
+        //             });
+        //         }}
+        //      />
+        //     )}
+        // />
     );
 };
 
@@ -51,7 +56,11 @@ export const screenOptions = navData => {
 };
 
 const styles = StyleSheet.create({
-    
+    screen: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 })
 
 export default PlacesListScreen;
