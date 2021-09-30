745cb98be993547b0f67136364ebe06ccad2c5c4
johnnie71 <johnnie.agonz71@gmail.com>
2021-09-30 19:25:22 -0400

    readability adjustments

--- components/PlaceItem.js
+++ components/PlaceItem.js
@@ -5,11 +5,11 @@ import Colors from '../constants/Colors';
 const PlaceItem = props => {
 
     return (
-        <TouchableOpacity onPress={props.onSelect} style={styles.placeItem} >
+        <TouchableOpacity onPress={props.onSelect} style={styles.placeItem}>
             <Image style={styles.image} source={{ uri: props.image }}/>
-            <View style={styles.infoContainer} >
-                <Text style={styles.title} >{props.title}</Text>
-                <Text style={styles.address} >{props.address}</Text>
+            <View style={styles.infoContainer}>
+                <Text style={styles.title}>{props.title}</Text>
+                <Text style={styles.address}>{props.address}</Text>
             </View>
         </TouchableOpacity>
     );
