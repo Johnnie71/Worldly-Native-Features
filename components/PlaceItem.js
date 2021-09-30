1583828c3b3663f21273f9787501468d2dc6b60d
johnnie71 <johnnie.agonz71@gmail.com>
2021-09-30 19:26:33 -0400

    css adjustments

--- components/PlaceItem.js
+++ components/PlaceItem.js
@@ -44,7 +44,7 @@ const styles = StyleSheet.create({
        width: 70,
        height: 70,
        borderRadius: 35,
-       backgroundColor: 'blue',
+       backgroundColor: '#ccc',
        borderColor: Colors.primary,
        borderWidth: 1
     }
