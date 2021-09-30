6efe28ef544580121d87cc17dcec036e42dbe6e5
johnnie71 <johnnie.agonz71@gmail.com>
2021-09-30 19:23:11 -0400

    semicolon

--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, Platform, FlatList, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
 
 import CustomHeaderButton from '../components/HeaderButton';
 import PlaceItem from '../components/PlaceItem';
