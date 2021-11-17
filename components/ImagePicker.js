7cffa49a89066c9789b3034378664c2386be013a
johnnie71 <johnnie.agonz71@gmail.com>
2021-11-17 17:53:47 -0500

    making some changes

--- components/ImagePicker.js
+++ components/ImagePicker.js
@@ -1,63 +1,65 @@
-import React from 'react';
-import { View, Button, Text, Image, StyleSheet, Alert } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-import * as Permissions from 'expo-permissions';
-import Colors from '../constants/Colors';
+import React from "react";
+import { View, Button, Text, Image, StyleSheet, Alert } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
+import Colors from "../constants/Colors";
 
+const ImgPicker = (props) => {
+	const verifyPermissions = async () => {
+		const result = await Permissions.askAsync(Permissions.CAMERA);
+		if (result.status != "granted") {
+			Alert.alert(
+				"Insufficient permissions!",
+				"You need to grant camera permissions to use this app.",
+				[{ text: "Okay" }]
+			);
+			return false;
+		}
+		return true;
+	};
 
-const ImgPicker = props => {
+	const takeImageHandler = async () => {
+		await verifyPermissions();
+		ImagePicker.launchCameraAsync();
+	};
 
-    const verifyPermissions = async () => {
-       const result = await Permissions.askAsync(Permissions.CAMERA);
-       if(result.status != 'granted'){
-            Alert.alert(
-                'Insufficient permissions!', 
-                'You need to grant camera permissions to use this app.', 
-                [{ text: "Okay"}]
-            );
-        return false;
-       }
-       return true;
-    };
-
-    const takeImageHandler = async () => { 
-        await verifyPermissions();
-        ImagePicker.launchCameraAsync();
-    };
-
-    return (
-        <View style={styles.imagePicker}>
-            <View style={styles.imagePreview}>
-                <Text>No image selected</Text>
-                <Image style={styles.image} />
-            </View>
-            <Button
-                title="Take Image"
-                color={Colors.primary}
-                onPress={takeImageHandler}
-            />
-        </View>
-    )
+	return (
+		<View style={styles.imagePicker}>
+			<View style={styles.imagePreview}>
+				<Text>No image selected</Text>
+				<Image style={styles.image} />
+			</View>
+			<Button
+				title="Take Image"
+				color={Colors.primary}
+				onPress={takeImageHandler}
+				style={styles.takeImage}
+			/>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-    imagePicker: {
-        alignItems: 'center'
-    },
-    imagePreview: {
-       width: '100%',
-       height: 200,
-       marginBottom: 10,
-       justifyContent: 'center',
-       alignItems: 'center',
-       borderColor: '#ccc',
-       borderWidth: 1
-    },
-    image: {
-        width: '100%',
-        height: '100%'
-    }
-
+	imagePicker: {
+		alignItems: "center",
+	},
+	imagePreview: {
+		width: "100%",
+		height: 200,
+		marginBottom: 10,
+		justifyContent: "center",
+		alignItems: "center",
+		borderColor: "#ccc",
+		borderWidth: 1,
+	},
+	image: {
+		width: "100%",
+		height: "100%",
+		margin: 10,
+	},
+	takeImage: {
+		marginBottom: 10,
+	},
 });
 
 export default ImgPicker;
--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,77 +1,78 @@
-import React, { useState } from 'react';
-import { 
-    ScrollView, 
-    View, 
-    Text, 
-    Button, 
-    TextInput, 
-    StyleSheet
-} from 'react-native';
-import { useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import {
+	ScrollView,
+	View,
+	Text,
+	Button,
+	TextInput,
+	StyleSheet,
+} from "react-native";
+import { useDispatch } from "react-redux";
 
-import Colors from '../constants/Colors';
-import * as placesActions from '../store/places-actions';
-import ImagePicker from '../components/ImagePicker';
+import Colors from "../constants/Colors";
+import * as placesActions from "../store/places-actions";
+import ImagePicker from "../components/ImagePicker";
 
-const NewPlaceScreen = props => {
+const NewPlaceScreen = (props) => {
+	const [titleValue, setTitleValue] = useState("");
 
-    const [titleValue, setTitleValue] = useState('');
+	const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+	const titleChangeHandler = (text) => {
+		setTitleValue(text);
+	};
 
-    const titleChangeHandler = text => {
-        setTitleValue(text);
-    };
+	const savePlaceHandler = () => {
+		dispatch(placesActions.addPlace(titleValue));
+		props.navigation.goBack();
+	};
 
-    const savePlaceHandler = () => {
-        dispatch(placesActions.addPlace(titleValue));
-        props.navigation.goBack();
-    };
-
-    return (
-        <ScrollView>
-            <View style={styles.form}>
-                <Text style={styles.label}>Title</Text>
-                <TextInput 
-                    style={styles.textInput} 
-                    onChangeText={titleChangeHandler}
-                    value={titleValue}
-                />
-                <ImagePicker />
-                <Button 
-                    style={styles.saveButton}
-                    title='Save Place' 
-                    color={Colors.primary} 
-                    onPress={savePlaceHandler}
-                />
-            </View>
-        </ScrollView>
-    )
-
+	return (
+		<ScrollView>
+			<View style={styles.form}>
+				<Text style={styles.label}>Title</Text>
+				<TextInput
+					style={styles.textInput}
+					onChangeText={titleChangeHandler}
+					value={titleValue}
+				/>
+				<ImagePicker />
+				<Button
+					style={styles.saveButton}
+					title="Save Place"
+					color={Colors.primary}
+					onPress={savePlaceHandler}
+				/>
+			</View>
+		</ScrollView>
+	);
 };
 
-export const screenOptions = navData => {
-
-    return {
-        headerTitle: 'Add Place',
-    }
-}
+export const screenOptions = (navData) => {
+	return {
+		headerTitle: "Add Place",
+	};
+};
 
 const styles = StyleSheet.create({
-    form: {
-        margin: 30
-    },
-    label: {
-        fontSize: 18,
-        marginBottom: 15
-    },
-    textInput: {
-        borderBottomColor: '#ccc',
-        borderBottomWidth: 1,
-        marginBottom: 15,
-        paddingVertical: 4,
-        paddingHorizontal: 2
-    }
+	form: {
+		margin: 30,
+		borderWidth: 1,
+	},
+	label: {
+		fontSize: 18,
+		marginBottom: 15,
+	},
+	textInput: {
+		borderBottomColor: "#ccc",
+		borderBottomWidth: 1,
+		marginBottom: "10%",
+		paddingVertical: 4,
+		paddingHorizontal: 2,
+	},
+	// saveButton: {
+	// 	marginTop: 10,
+	// },
 });
 
 export default NewPlaceScreen;
