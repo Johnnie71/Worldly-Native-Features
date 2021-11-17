dfb33f1a6dc4a606ac5f0a0108619e8dc4761ce1
johnnie71 <johnnie.agonz71@gmail.com>
2021-11-17 18:15:12 -0500

    formatting

--- screens/PlaceDetailScreen.js
+++ screens/PlaceDetailScreen.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-const PlaceDetailScreen = props => {
-
-    return (
-        <View>
-            <Text>Place Detail Screen</Text>
-        </View>
-    )
-
+const PlaceDetailScreen = (props) => {
+	return (
+		<View>
+			<Text>Place Detail Screen</Text>
+		</View>
+	);
 };
 
-export const screenOptions = navData => {
-    return {
-        headerTitle: navData.navigation.getParam('placeTitle')
-    }
+export const screenOptions = (navData) => {
+	return {
+		headerTitle: navData.navigation.getParam("placeTitle"),
+	};
 };
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
 
 export default PlaceDetailScreen;
