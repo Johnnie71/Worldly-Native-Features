f7f2123cd4b167578a2fb5a8764c39daa2c8fae4
johnnie71 <johnnie.agonz71@gmail.com>
2021-11-26 16:51:25 -0500

    added image reducer

--- store/places-reducer.js
+++ store/places-reducer.js
@@ -1,19 +1,22 @@
 import { ADD_PLACE } from "./places-actions";
-import Place from '../models/place';
+import Place from "../models/place";
 
 const initialState = {
-    places: []
+	places: [],
 };
 
 export default (state = initialState, action) => {
-    switch (action.type) {
-        case ADD_PLACE:
-            const newPlace = new Place(new Date().toString(), action.placeData.title);
-            return {
-                places: state.places.concat(newPlace)
-            };
-            default:
-                return state;
-    }
-    
+	switch (action.type) {
+		case ADD_PLACE:
+			const newPlace = new Place(
+				new Date().toString(),
+				action.placeData.title,
+				action.placeData.imageUri
+			);
+			return {
+				places: state.places.concat(newPlace),
+			};
+		default:
+			return state;
+	}
 };
