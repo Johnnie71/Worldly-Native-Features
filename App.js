5d7d986fe488e262dc603dfe383d5599bd6b289b
johnnie71 <johnnie.agonz71@gmail.com>
2021-11-29 18:47:56 -0500

    initializing the database upon startup

--- App.js
+++ App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import ReduxThunk from 'redux-thunk';
-import PlacesNavigator from './navigation/PlacesNavigator';
-import placesReducer from './store/places-reducer';
+import React from "react";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import ReduxThunk from "redux-thunk";
+import PlacesNavigator from "./navigation/PlacesNavigator";
+import placesReducer from "./store/places-reducer";
+import { init } from "./helpers/db";
 
+init(); // initializes the database upon startup!
 
 const rootReducer = combineReducers({
-  places: placesReducer
+	places: placesReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default function App() {
-
-  return (
-    <Provider store={store}>
-      <PlacesNavigator />
-    </Provider>
-    
-  )
+	return (
+		<Provider store={store}>
+			<PlacesNavigator />
+		</Provider>
+	);
 }
-
