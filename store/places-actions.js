0d1b32e415af9257d73f6ef6fa5773a3f90be121
johnnie71 <johnnie.agonz71@gmail.com>
2021-11-30 18:34:00 -0500

    setting action creator

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,7 +1,8 @@
 import * as FileSystem from "expo-file-system";
+import { insertPlace } from "../helpers/db";
 
 export const ADD_PLACE = "ADD_PLACE";
-import { insertPlace } from "../helpers/db";
+export const SET_PLACES = "SET_PLACES";
 
 export const addPlace = (title, image) => {
 	return async (dispatch) => {
