95527679c133a8288440070bf6d22622e8a34b64
johnnie71 <johnnie.agonz71@gmail.com>
2021-12-01 18:43:46 -0500

    rid of console.logs

--- store/places-actions.js
+++ store/places-actions.js
@@ -21,7 +21,6 @@ export const addPlace = (title, image) => {
 				15.6,
 				12.3
 			);
-			console.log(dbResult);
 			dispatch({
 				type: ADD_PLACE,
 				placeData: { id: dbResult.insertId, title: title, image: newPath },
@@ -37,7 +36,6 @@ export const loadPLaces = () => {
 	return async (dispatch) => {
 		try {
 			const dbResult = await fetchPlaces();
-			console.log(dbResult);
 			dispatch({ type: SET_PLACES, places: dbResult.rows._array });
 		} catch (err) {
 			console.log(err);
