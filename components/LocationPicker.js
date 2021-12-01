0912ec63aa180f5a0836073cf8ebb1916396d014
johnnie71 <johnnie.agonz71@gmail.com>
2021-12-01 18:12:33 -0500

    set up

---
+++ components/LocationPicker.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+	View,
+	Button,
+	Text,
+	ActivityIndicator,
+	Alert,
+	StyleSheet,
+} from "react-native";
+
+const LocationPicker = () => {
+	return (
+		<View>
+			<View>
+				<Text>No location chosen yet!</Text>
+			</View>
+			<Button />
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default LocationPicker;
