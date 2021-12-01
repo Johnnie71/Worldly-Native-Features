ebf83805c99f9dc902e7bfe3b006663916a2698a
johnnie71 <johnnie.agonz71@gmail.com>
2021-12-01 18:13:56 -0500

    getlocationhandler on press

--- components/LocationPicker.js
+++ components/LocationPicker.js
@@ -10,12 +10,18 @@ import {
 import Colors from "../constants/Colors";
 
 const LocationPicker = () => {
+	const getLocationHandler = () => {};
+
 	return (
 		<View>
 			<View>
 				<Text>No location chosen yet!</Text>
 			</View>
-			<Button title="Get user location" color={Colors.primary} />
+			<Button
+				title="Get user location"
+				color={Colors.primary}
+				onPress={getLocationHandler}
+			/>
 		</View>
 	);
 };
